5a2011d35dbb96795353aceaca313fde3ba16780
lllena <51027280+lllena@users.noreply.github.com>
2020-04-17 22:16:27 +0300

    Add files via upload

--- src/domain/Exercise.java
+++ src/domain/Exercise.java
@@ -1,7 +1,35 @@
 package domain;
 
+/**
+ * Main class for working with numbers
+ * @author admin
+ */
 public class Exercise {
-    public static float Calculate(int R){
-        return (float) (2*Math.PI*Math.sqrt(R));
+
+    /**
+     * Method that transletes a decimal number to binary
+     * @param R integer with whitch action will be performed
+     * @return the specified number in binary notation as a string
+     */
+    public static String CalculateBinarySystem(int R){
+        return (String) (Integer.toBinaryString((int) R));
+    }
+    
+    /**
+     * Method that transletes a decimal number to octal
+     * @param R integer with whitch action will be performed
+     *  @return the specified number in octal notation as a string
+     */
+    public static String CalculateOctalSystem(int R){
+        return (String) (Integer.toOctalString((int) R));
+    }
+    
+    /**
+     * Method that transletes a decimal number to hexadecimal
+     * @param R integer with whitch action will be performed
+     *  @return the specified number in hexadecimal notation as a string
+     */
+    public static String CalculateHexadecimalSystem(int R){
+        return (String) (Integer.toHexString((int) R));
     }
 }
--- src/test/TestResult.java
+++ src/test/TestResult.java
@@ -1,12 +1,22 @@
 package test;
-
 import domain.Exercise;
+
+/**
+ * Test class to test the functionality of tne main class
+ * @author admin
+ */
 
 public class TestResult {
 
     public static void main(String[] args) {
 
-        int R=10;
-        System.out.println("Area of a circle for R="+R+"m is "+Exercise.Calculate(R)+"m"+'\u00B2');
+        int R=158635;
+        if(R>=100000 && R<1000000)
+            {
+            System.out.println("Число "+R+" в двійковій системі числення: "+Exercise.CalculateBinarySystem(R)+"\nв восьмеричній системі числення: "+Exercise.CalculateOctalSystem(R)+"\nв шістнадцятиричній системі числення: "+Exercise.CalculateHexadecimalSystem(R));
+            }
+        else{
+            System.out.println("Число R должно быть шестизначным!");
+            }
     }
 }
